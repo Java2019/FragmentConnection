f0d09f40031b81becb89515cbad910de2bb8a6d4
java2019 <java_2019@mail.ru>
2017-08-02 11:56:04 +0300

    init

--- app/src/main/java/com/nemo/fragmentconnection/TwoFragment.java
+++ app/src/main/java/com/nemo/fragmentconnection/TwoFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -26,6 +27,11 @@ public class TwoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+    public void setText(String item){
+        TextView textView = (TextView)getView().findViewById(R.id.text);
+        textView.setText(item);
     }
 
 }
